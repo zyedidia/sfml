978d1d92c4c937a105fd76776d31a4a1b7e6f499
Cosku Bas <cosku.bas@gmail.com>
2016-05-05 08:21:25 +0300

    System window

---
+++ v2.3/sfml/systemWindow.go
@@ -0,0 +1,26 @@
+package sfml
+
+type SystemWindow interface{
+	Close()
+	IsOpen() bool
+	GetSettings() *ContextSettings
+	PollEvent() *Event
+	WaitEvent() *Event
+	GetPosition() Vector2i
+	SetPosition(position Vector2i)
+	GetSize() Vector2u
+	SetSize(size Vector2u)
+	SetTitle(title string)
+	SetIcon(width, height uint, pixels []uint8)
+	SetVisible(visible bool)
+	SetMouseCursorVisible(visible bool)
+	SetVerticalSyncEnabled(enabled bool)
+	SetKeyRepeatEnabled(enabled bool)
+	SetActive(active bool) bool
+	RequestFocus()
+	HasFocus() bool
+	Display()
+	SetFramerateLimit(limit uint)
+	SetJoystickThreshold(treshold float32)
+	GetSystemHandle() WindowHandle
+}
