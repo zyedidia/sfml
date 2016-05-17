89d75e01d0de3a546bb5054fe5dfede11b5aa886
Cosku Bas <cosku.bas@gmail.com>
2016-05-17 12:43:14 +0300

    Fix #4, Update #1 and #5

--- v2.3/sfml/color.go
+++ v2.3/sfml/color.go
@@ -4,15 +4,15 @@ package sfml
 import "C"
 
 var (
-	ColorBlack       = *goColor(&C.sfBlack)
-	ColorWhite       = *goColor(&C.sfWhite)
-	ColorRed         = *goColor(&C.sfRed)
-	ColorGreen       = *goColor(&C.sfGreen)
-	ColorBlue        = *goColor(&C.sfBlue)
-	ColorYellow      = *goColor(&C.sfYellow)
-	ColorMagenta     = *goColor(&C.sfMagenta)
-	ColorCyan        = *goColor(&C.sfCyan)
-	ColorTransparent = *goColor(&C.sfTransparent)
+	ColorBlack       = Color{0, 0, 0, 255}
+	ColorWhite       = Color{255, 255, 255, 255}
+	ColorRed         = Color{255, 0, 0, 255}
+	ColorGreen       = Color{0, 255, 0, 255}
+	ColorBlue        = Color{0, 0, 255, 255}
+	ColorYellow      = Color{255, 255, 0, 255}
+	ColorMagenta     = Color{255, 0, 255, 255}
+	ColorCyan        = Color{0, 255, 255, 255}
+	ColorTransparent = Color{255, 255, 255, 0}
 )
 
 type Color struct {
@@ -27,23 +27,27 @@ func CreateColorFromInteger(color uint) Color {
 	return *goColor(&r)
 }
 
-func (c Color) Add(cc Color) Color {
-	r := C.sfColor_add(cColor(&c), cColor(&cc))
+func (c Color) Add(color Color) Color {
+	r := C.sfColor_add(cColor(&c), cColor(&color))
 	return *goColor(&r)
 }
 
-func (c Color) Subtract(cc Color) Color {
-	r := C.sfColor_subtract(cColor(&c), cColor(&cc))
+func (c Color) Subtract(color Color) Color {
+	r := C.sfColor_subtract(cColor(&c), cColor(&color))
 	return *goColor(&r)
 }
 
-func (c Color) Modulate(cc Color) Color {
-	r := C.sfColor_modulate(cColor(&c), cColor(&cc))
+func (c Color) Modulate(color Color) Color {
+	r := C.sfColor_modulate(cColor(&c), cColor(&color))
 	return *goColor(&r)
 }
 
 func (c Color) ToInteger() uint {
 	return uint(C.sfColor_toInteger(cColor(&c)))
+}
+
+func (c Color) IsEqual(color Color) bool {
+	return c.R == color.R && c.G == color.G && c.B == color.B && c.A == color.A
 }
 
 func goColor(c *C.sfColor) *Color {
